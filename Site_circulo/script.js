c704e61a2ae9392ec1fbcbc4c4926afc92540983
Rafael Assis Carvalho Lacerda <rafaelassisclacerda@gmail.com>
2025-03-27 16:46:37 +0000

    versao final

--- Site_circulo/script.js
+++ Site_circulo/script.js
@@ -5,61 +5,49 @@ var vol;
 const valorRaio = document.getElementById('valorRaio');
 const bcalcular = document.getElementById('bcalcular');
 
+function printar(){
+    const valorComp = document.getElementById('comprimento');
+    const valorArea = document.getElementById('area');
+    const valorVolume = document.getElementById('volume');
+
+    valorComp.innerHTML = `Comprimento: <span class="valor">${comp.toFixed(2)}</span>`;
+    valorArea.innerHTML = `Área: <span class="valor">${area.toFixed(2)}</span>`;
+    valorVolume.innerHTML = `Volume: <span class="valor">${vol.toFixed(2)}</span>`;
+}
 
 function calcular(CAV, raio){
     raio = parseFloat(raio);
+    var C;
+    var A;
+    var V;
     switch(CAV){
-        case 'C':
-            var C;
+        case 'C': 
             C = 2*raio*Math.PI;
-            console.log(`Comprimento calculado: ${C}`);
             return C;
         case 'A':
-            var A;
             A = Math.pow(raio, 2) * Math.PI;
-            console.log(`Área calculada: ${A}`);
             return A;
         case 'V':
-            var V;
-            V =  Math.pow(raio, 3) * Math.PI * (3/4);
-            console.log(`Volume calculado: ${V}`);
+            V =  Math.pow(raio, 3) * Math.PI * (4/3);
             return V;
             break;
     }
 }
 
-function printAll(){
-    const Pcomp = document.getElementById('comprimento');
-    const Parea = document.getElementById('area');
-    const Pvol = document.getElementById('volume');
-    Pcomp.innerHTML = `Comprimento: <span class="valor">${comp.toFixed(2)}</span>`;
-    Parea.innerHTML = `Área: <span class="valor">${area.toFixed(2)}</span>`;
-    Pvol.innerHTML = `Volume: <span class="valor">${vol.toFixed(2)}`;
-
-}
-
-function lerRaio() {
+function Raio() {
     let raio = parseFloat(valorRaio.value);
    
     comp = calcular('C', raio);
     area = calcular('A', raio);
     vol = calcular('V', raio);
-    printAll();
+    printar();
     
 }
 
-
-bcalcular.addEventListener('mouseup', function(){
-    bcalcular.classList.remove('press');
-})
 bcalcular.addEventListener('mousedown', function(){
     bcalcular.classList.add('press');
-    lerRaio();
+    Raio();
 })
-
-valorRaio.addEventListener('keypress', function(event){
-    if (event.key === 'Enter'){
-        lerRaio();
-    }
-
+bcalcular.addEventListener('mouseup', function(){
+    bcalcular.classList.remove('press');
 })
